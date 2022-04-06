eaee86fd869e76393e38bfe76e0b3ddc0b39b4dd
Fernando Zalazar <f.zalazar@live.com>
2022-04-06 10:49:59 -0300

    practica1.2

---
+++ algoritmo.js
@@ -0,0 +1,11 @@
+console.log("hola")
+
+document.write('<iframe width="560" height="315" src="https://www.youtube.com/embed/X0-xje2YrvE" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+
+document.write('<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d167998.12179446057!2d2.206952164367434!3d48.85877000312214!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47e66e1f06e2b70f%3A0x40b82c3688c9460!2zUGFyw61zLCBGcmFuY2lh!5e0!3m2!1ses!2sar!4v1648816975301!5m2!1ses!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>')
+
+document.write('<iframe src="https://cdn.pixabay.com/audio/2022/03/23/audio_07b2a04be3.mp3" frameborder="0"></iframe>')
+
+
+
+document.write('<iframe src="https://www.mercadolibre.com.ar/" frameborder="0"></iframe>')
